72707557cc328e5f8342b795b49af6eadd58df2f
Faeez Miller <faeezmiller8@gmail.com>
2023-01-09 16:31:34 +0200

    new

---
+++ Documents/vueportfolio/src/App.vue
@@ -0,0 +1,100 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: black;
+}
+
+    body,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+      font-family: "Montserrat", "sans-serif"
+    }
+
+    .w3-row-padding img {
+      margin-bottom: 12px
+    }
+
+    /* Set the width of the sidebar to 120px */
+    .w3-sidebar {
+      width: 120px;
+      background: #222;
+    }
+
+    /* Add a left margin to the "page content" that matches the width of the sidebar (120px) */
+    #main {
+      margin-left: 120px
+    }
+
+    /* Remove margins from "page content" on small screens */
+    @media only screen and (max-width: 600px) {
+      #main {
+        margin-left: 0
+      }
+    }
+  
+    *,
+*::after,
+*::before{
+    padding: 0%;
+    margin: 0%;
+    box-sizing: border-box;}
+
+   h1{
+  cursor: pointer;
+  font-size: 20vw;
+  color: transparent;
+  background:url(https://i.postimg.cc/zXdg1nHp/textbg.jpg);
+  background-size: 40%;
+  background-position: 50% 50%;
+  -webkit-background-clip: text;
+}
+.picture2{
+  height: 90%;
+  width: 95%;
+}
+.picture3{
+  height: 90%;
+  width: 95%;
+}
+.nasa{
+  height: 200px ;
+}
+#home h1 {
+  text-align: center;
+  animation: 2s title infinite;
+}
+@keyframes title {
+  0% {
+      opacity: 0;
+      transform: rotateX(90def);
+  }
+  50% {
+      opacity: 1;
+      transform: rotateX(720deg);
+  }
+  100% {
+      opacity: 1;
+      transform: rotateX(720deg);
+  }
+}
+
+
+
+
+</style>
---
+++ Documents/vueportfolio/src/views/HomeView.vue
@@ -0,0 +1,44 @@
+<template>
+  <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+  </head>
+
+
+<!-- Icon Bar (Sidebar - hidden on small screens) -->
+<nav class="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center">
+  <!-- Avatar image in top left corner -->
+  <img src="https://i.postimg.cc/0j6Ss9NY/mr-robot.jpg" style="width:100%">
+
+
+  <a href="HomeView.vue" class="w3-bar-item w3-button w3-padding-large w3-black">
+    <i class="fa fa-home w3-xxlarge"></i>
+    <p>HOME</p>
+  </a>
+  <a href="AboutView.vue" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+    <i class="fa fa-user w3-xxlarge"></i>
+    <p>ABOUT</p>
+  </a>
+  <a href="TestimonialsView.vue" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+    <i class="fa-solid fa-comments w3-xxlarge"></i>
+    <p>TESTIMONIALS</p>
+  </a>
+  <a href="ProjectsView.vue" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+    <i class="fa-solid fa-briefcase w3-xxlarge"></i>
+    <p>PROJECTS</p>
+  </a>
+  <a href="PhotosView.vue" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+    <i class="fa-solid fa-image w3-xxlarge"></i>
+    <p>PHOTOS</p>
+  </a>
+  <a href="ContactView.vue" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+    <i class="fa fa-envelope w3-xxlarge"></i>
+    <p>CONTACT</p>
+  </a>
+</nav>
+
+
+</template>
+
+
